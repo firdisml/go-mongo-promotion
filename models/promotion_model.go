5282488eb880fd89b96b4c191128d8f7edf38e44
Firdaus Ismail <44713893+firdisml@users.noreply.github.com>
2023-02-21 14:49:23 +0800

    Added picture column on promotion update route

--- models/promotion_model.go
+++ models/promotion_model.go
@@ -20,14 +20,13 @@ type Promotion struct {
 }
 
 type PromotionUpdate struct {
-	Id       primitive.ObjectID `json:"id,omitempty"`
-	Title    string             `json:"title,omitempty" validate:"required"`
-	Category string             `json:"category,omitempty" validate:"required"`
-	Shop     string             `json:"shop,omitempty" validate:"required"`
-	State    string             `json:"state,omitempty" validate:"required"`
-	Link     string             `json:"link,omitempty" validate:"required"`
-	Start    time.Time          `json:"start" validate:"required"`
-	End      time.Time          `json:"end" validate:"required"`
-	Visible  *bool              `json:"visible" validate:"required"`
-	Picture  *bool              `json:"picture" validate:"required"`
+	Title    string    `json:"title,omitempty" validate:"required"`
+	Category string    `json:"category,omitempty" validate:"required"`
+	Shop     string    `json:"shop,omitempty" validate:"required"`
+	State    string    `json:"state,omitempty" validate:"required"`
+	Link     string    `json:"link,omitempty" validate:"required"`
+	Start    time.Time `json:"start" validate:"required"`
+	End      time.Time `json:"end" validate:"required"`
+	Visible  *bool     `json:"visible" validate:"required"`
+	Picture  *bool     `json:"picture" validate:"required"`
 }
